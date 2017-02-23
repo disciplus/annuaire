110d9c276bc975dcc8ae6cfa99004d0ce83ff237
Disciplus <github@disciplus.33mail.com>
2017-02-23 12:01:11 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 
 try
 {
-  $bdd = new PDO('mysql:host=localhost;dbname=annuaire', troll, anpwzbox);
+  $bdd = new PDO('mysql:host=localhost;dbname=annuaire', troll, xxxxxxx);
 }
 catch(Exception $e)
 {
