4aa52623343306afa93ac2486951172d1a5179fd
disciplus <github@disciplus.33mail.com>
2017-02-23 09:45:15 +0100

    Mon prmeier commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <title>Accueil</title>
+    <meta charset="utf-8">
+    <meta name="author" content="Disciplus Simplex"/>
+    <meta name="keywords" content="PHP, Base annuaire"/>
+    <link rel="icon" type="image/x-icone" href="images/pirate.ico"/>
+    <!-- <link rel="stylesheet" type="text/css" href="css/knaccs.css" />   -->
+    <link rel="stylesheet" type="text/css" href="css/standard.css" />
+</head>
+
+  <body> 
+    <h1>Accueil annuaire</h1>
+
+  <a href="saisie_annuaire.php">Saisie</a><br />
+  <a href="consultation_annuaire.php">Consultation</a>
+  </body>
+</html>
